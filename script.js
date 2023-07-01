c2b21d053c62508d335cf78b13e153b200a13032
Hayespace <ben.hayes@hotmail.com>
2023-07-01 10:03:34 +0000

    assign sounds to keyboard strokes and animation

--- script.js
+++ script.js
@@ -16,6 +16,28 @@ function removeActiveClasses() {
     })
 }
 
+// Keyboard functions //
+
+const pads = Array.from(document.querySelectorAll('.btn'));
+pads.forEach(btn => btn.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const button = document.querySelector(`.btn[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    button.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();  
+    
+}
+
 // Sounds to button on click //
 
 const kick = new Audio();
