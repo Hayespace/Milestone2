f7a024b9188240f80e9daf73240da45ae7b4b3b4
Hayespace <ben.hayes@hotmail.com>
2023-07-01 09:42:19 +0000

    add panel active animation to js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Scrolling panels functions //
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+
+    })
+}
+
