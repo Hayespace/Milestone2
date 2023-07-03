26386315d40daa596b11a11aeabd65e89c05e464
Hayespace <ben.hayes@hotmail.com>
2023-07-03 13:36:47 +0000

    remove onClick from HTMl and replace with js

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ panels.forEach(panel => {
       panel.addEventListener('click', () => {
       removeActiveClasses()
       panel.classList.add('active')
-     })
+     });
+});
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => {
+      btn.addEventListener('mousedown', event => {
+            const keyCode = event.currentTarget.getAttribute('data-key');
+            playSound(keyCode);
+      });
+      btn.addEventListener('mouseup', event => {
+            const keyCode = event.currentTarget.getAttribute('data-key');
+            buttonAnimate(keyCode);
+      });
 })
 
 function removeActiveClasses() {
@@ -36,32 +48,4 @@ function buttonAnimate(keyCode){
 
 
 
-// Sounds to button on click
-
-const kick = new Audio();
-kick.src = 'assets/sounds/library-1/Kick.wav';
-
-const snare = new Audio();
-snare.src = 'assets/sounds/library-1/snare.wav';
-
-const chh = new Audio();
-chh.src = 'assets/sounds/library-1/ClosedHH.wav';
-
-const hh = new Audio();
-hh.src = 'assets/sounds/library-1/HH.wav';
-
-const crash = new Audio();
-crash.src = 'assets/sounds/library-1/Crash.wav';
-
-const clap = new Audio();
-clap.src = 'assets/sounds/library-1/clap.wav';
-
-const conga = new Audio();
-conga.src = 'assets/sounds/library-1/Conga.wav';
-
-const cowbell = new Audio();
-cowbell.src = 'assets/sounds/library-1/Cowbell.wav';
-
-const shaker = new Audio();
-shaker.src = 'assets/sounds/library-1/Shaker.wav';
 
