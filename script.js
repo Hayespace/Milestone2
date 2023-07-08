125a43582c8734aa49757708193f0136c9124670
Hayespace <ben.hayes@hotmail.com>
2023-07-08 15:28:16 +0000

    write piano function to play sounds

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ panels.forEach(panel => {
      });
 });
 
+function removeActiveClasses() {
+      panels.forEach(panel => {
+      panel.classList.remove('active')});
+};
+
+// Mouse click playing
+
 const btns = document.querySelectorAll('.btn');
 btns.forEach(btn => {
       btn.addEventListener('mousedown', event => {
@@ -18,13 +25,9 @@ btns.forEach(btn => {
       btn.addEventListener('mouseup', event => {
             const keyCode = event.currentTarget.getAttribute('data-key');
             buttonAnimate(keyCode);
-      });
-})
+      });     
+});
 
-function removeActiveClasses() {
-      panels.forEach(panel => {
-      panel.classList.remove('active')});
-}
 
 // Key press audio and button animation
 
@@ -38,33 +41,47 @@ function playSound(keyCode){
     btndown.classList.add('playing');
     audio.currentTime = 0;
     audio.play();  
-}
+};
 
 function buttonAnimate(keyCode){
-      console.log('test', `.btn[data-key="${keyCode}"]`)
-   const btnup = document.querySelector(`.btn[data-key="${keyCode}"]`);
-   btnup.classList.remove('playing');
-}
+    const btnup = document.querySelector(`.btn[data-key="${keyCode}"]`);
+
+    btnup.classList.remove('playing');
+};
 
 
 // Piano Keyboard
 
 const keys = document.querySelectorAll('.key');
-btns.forEach(key => {
+keys.forEach(key => {
       key.addEventListener('mousedown', event => {
             const keyCode = event.currentTarget.getAttribute('data-key');
-            playSound(keyCode);
+            playPiano(keyCode);
       });
       key.addEventListener('mouseup', event => {
             const keyCode = event.currentTarget.getAttribute('data-key');
-            buttonAnimate(keyCode);
-      });
-})
-
-function removeActiveClasses() {
-      panels.forEach(panel => {
-      panel.classList.remove('active')});
-}
+            keyAnimate(keyCode);
+      });     
+});
 
 
+// Key press audio and key animation
+
+window.addEventListener('keydown', event => playPiano(event.keyCode));
+window.addEventListener('keyup', event => keyAnimate(event.keyCode)); 
+
+function playPiano(keyCode){
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const keyDown = document.querySelector(`.active .key[data-key="${keyCode}"]`);
+    
+    keyDown.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();  
+};
+
+function keyAnimateAnimate(keyCode){
+    const keyUp = document.querySelector(`.key[data-key="${keyCode}"]`);
+
+    keyUp.classList.remove('playing');
+};
 
