b54f46b2387c6540507f353fb019fedba9662937
Hayespace <ben.hayes@hotmail.com>
2023-07-12 14:33:58 +0000

    add media recorder

--- script.js
+++ script.js
@@ -85,7 +85,16 @@ function keyAnimate(keyCode){
     keyUp.classList.remove('key-playing');
 };
 
+var btnStart = document.querySelector('button[name="record"]');
+var btnStop = document.querySelector('button[name="stop"]');
+var audio = document.querySelector('#audio');
 
-
-
+btnStart.addEventListener('click', async () => {
+      let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
+      let mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      let chunks = [];
+      mediaRecorder.ondataavailable = (e)=>{
+           chunks.push(e.data);
+      }
 
