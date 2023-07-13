0d88f2ac5daaf367cb4cc5cddfbe4fa236f322d7
Hayespace <ben.hayes@hotmail.com>
2023-07-13 15:19:05 +0000

    add audio record button to first panel

--- script.js
+++ script.js
@@ -117,4 +117,33 @@ btnStart.addEventListener('click', async () => {
 });
 
 
+// Audio Recorder
 
+var btnStart = document.querySelector('button[name="record"]');
+var btnStop = document.querySelector('button[name="stop"]');
+var audio = document.querySelector('#audio');
+
+btnStart.addEventListener('click', async () => {
+      let stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
+      let mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      let chunks = [];
+      mediaRecorder.ondataavailable = (e)=>{
+           chunks.push(e.data);
+      }
+      
+       mediaRecorder.onerror = (e)=>{
+            alert(e.error);
+       }
+
+ mediaRecorder.onstop = (e)=>{
+      let blod = new Blob(chunks);
+      let url = URL.createObjectURL(blod);
+      audio.src = url;
+ }
+
+ btnStop.addEventListener('click',()=>{
+      mediaRecorder.stop();
+ })
+
+});
